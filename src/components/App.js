5846a6e10ffad4f0abc9182cdd397acd771acae9
celeste99 <xueyuanxin94@gmail.com>
2019-04-26 21:34:39 -0400

    isLoggedin, getLogin

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
 import { TopBar } from './TopBar';
 import { Main } from './Main';
+import { TOKEN_KEY } from '../constants';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <TopBar/>
-        <Main/>
-      </div>
-    );
-  }
+    state = {
+        isLoggedIn: Boolean(localStorage.getItem(TOKEN_KEY)),
+    }
+
+    handleSuccessfulLogin = (token) => {
+        localStorage.setItem(TOKEN_KEY, token);
+        this.setState( {isLoggedIn: true} );
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <TopBar/>
+                <Main handleSuccessfulLogin={this.handleSuccessfulLogin} isLoggedIn={this.state.isLoggedIn}/>
+            </div>
+        );
+    }
 }
 
 export default App;
